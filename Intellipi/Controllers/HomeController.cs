2e0493096a475e19dffb96153e38b52b74c733ce
TasveerS <tasveersingh@hotmail.com>
2018-09-13 21:50:50 +0200

    Add project files.

---
+++ Intellipi/Controllers/HomeController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ImageResizer;
+using ApexDev.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.ProjectOxford.Vision;
+
+namespace ApexDev.Controllers
+{
+    public class HomeController : Controller
+    {
+        private bool HasMatchingMetadata(CloudBlockBlob blob, string term)
+        {
+            foreach (var item in blob.Metadata)
+            {
+                if (item.Key.StartsWith("Tag") && item.Value.Equals(term, StringComparison.InvariantCultureIgnoreCase))
+                    return true;
+            }
+
+            return false;
+        }
+        public ActionResult Index(string id)
+        {
+         
+            CloudStorageAccount account = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            CloudBlobClient client = account.CreateCloudBlobClient();
+            CloudBlobContainer container = client.GetContainerReference("photos");
+            List<BlobInfo> blobs = new List<BlobInfo>();
+
+            foreach (IListBlobItem item in container.ListBlobs())
+            {
+                var blob = item as CloudBlockBlob;
+
+                if (blob != null)
+                {
+                    blob.FetchAttributes(); 
+
+                    if (String.IsNullOrEmpty(id) || HasMatchingMetadata(blob, id))
+                    {
+                        var caption = blob.Metadata.ContainsKey("Caption") ? blob.Metadata["Caption"] : blob.Name;
+
+                        blobs.Add(new BlobInfo()
+                        {
+                            ImageUri = blob.Uri.ToString(),
+                            ThumbnailUri = blob.Uri.ToString().Replace("/photos/", "/thumbnails/"),
+                            Caption = caption
+                        });
+                    }
+                }
+            }
+
+            ViewBag.Blobs = blobs.ToArray();
+            ViewBag.Search = id; 
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                
+                if (!file.ContentType.StartsWith("image"))
+                {
+                    TempData["Message"] = "Only image files may be uploaded";
+                }
+                else
+                {
+                    try
+                    {
+
+                        CloudStorageAccount account = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+                        CloudBlobClient client = account.CreateCloudBlobClient();
+                        CloudBlobContainer container = client.GetContainerReference("photos");
+                        CloudBlockBlob photo = container.GetBlockBlobReference(Path.GetFileName(file.FileName));
+                        await photo.UploadFromStreamAsync(file.InputStream);
+
+
+                        using (var outputStream = new MemoryStream())
+                        {
+                            file.InputStream.Seek(0L, SeekOrigin.Begin);
+                            var settings = new ResizeSettings { MaxWidth = 192 };
+                            ImageBuilder.Current.Build(file.InputStream, outputStream, settings);
+                            outputStream.Seek(0L, SeekOrigin.Begin);
+                            container = client.GetContainerReference("thumbnails");
+                            CloudBlockBlob thumbnail = container.GetBlockBlobReference(Path.GetFileName(file.FileName));
+                            await thumbnail.UploadFromStreamAsync(outputStream);
+                        }
+
+
+                        // Computer Vision API
+                        VisionServiceClient vision = new VisionServiceClient(
+                            ConfigurationManager.AppSettings["SubscriptionKey"],
+                            ConfigurationManager.AppSettings["VisionEndpoint"]
+                        );
+
+                        VisualFeature[] features = new VisualFeature[] { VisualFeature.Description };
+                        var result = await vision.AnalyzeImageAsync(photo.Uri.ToString(), features);
+
+                       
+                        photo.Metadata.Add("Caption", result.Description.Captions[0].Text);
+
+                        for (int i = 0; i < result.Description.Tags.Length; i++)
+                        {
+                            string key = String.Format("Tag{0}", i);
+                            photo.Metadata.Add(key, result.Description.Tags[i]);
+                        }
+
+                        await photo.SetMetadataAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        
+                        TempData["Message"] = ex.Message;
+                    }
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Search(string term)
+        {
+            return RedirectToAction("Index", new { id = term });
+        }
+
+    }
+}
